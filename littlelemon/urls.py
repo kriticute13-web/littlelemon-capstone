0700f9ceadf1a972e92f9fcba8017dae404b9188
Kriti Tripathi <Krititripathi38@gmail.com>
2026-06-09 04:34:28 +0530

    Fix homepage routing and complete capstone

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('restaurant.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
